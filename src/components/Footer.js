5df8ade52feb50f3b788acd97d8c1025d2665e86
arpit sharma <itssharma6074@gmail.com>
2025-06-23 00:56:46 +0530

    updated flaws in the links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/its_arpit_sharma"
+                href="https://www.instagram.com/its_me_arpitsharma"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
